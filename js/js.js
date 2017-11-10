8b2e654d79c8e53c92ec15f0cdcac1e3a514235d
wojtekpac <w.pac@meatnet.pl>
2017-11-10 13:47:02 +0100

    dropdown clone + counter

--- js/js.js
+++ js/js.js
@@ -2,8 +2,11 @@
  * Created by wojciechpac on 07.11.2017.
  */
 $(function () {
+    var $start_counter = $("#event-start"),
+        counts = [ 0 ];
+
     $(".draggable").draggable({
-        grid: [20, 20],
+        grid: [10, 10],
         appendTo: '#droppable',
         containment: "window",
         cursor: 'move',
@@ -16,8 +19,19 @@ $(function () {
         accept: ".draggable",
         drop: function (event, ui) {
             ui.helper.clone().appendTo('#droppable');
+            counts[0]++;
+            updateCounterStatus($start_counter, counts[0]);
+            $("#droppable .draggable").addClass("item");
+            $(".item").removeClass("ui-draggable draggable");
+            $('.item').draggable({
+                containment: 'parent'
+            });
         }
     });
 
-
+    function updateCounterStatus($event_counter, new_count) {
+        $("span.count", $event_counter).text(new_count);
+    }
 });
+
+
